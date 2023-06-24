ca31ac32dc2473f6b8522b07b2b04dd4cd949e02
petethomasmusique <petethomasmusique@gmail.com>
2023-06-24 21:54:31 +0100

    replace editor with monaco

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,23 +1,65 @@
 <script lang="ts">
-    import CodeMirror from '@joshnuss/svelte-codemirror'
-    import {EditorView, basicSetup, closeBrackets} from "codemirror"
-  
-    console.log(closeBrackets)
-    const options = {
-		mode: "javascript",
-		lineNumbers: false,
-        theme: "material",
-        tabSize: 2,
-        lineWrapping: true,
-        height: "100%",
-        // keyMaps: [standardKeymap],
-        // extensions: basicSetup
-	}
-	
-	let code = `// Welcome to Zen!`
-	let editor
+    import loader from '@monaco-editor/loader';
+    import { onDestroy, onMount } from 'svelte';
+    import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-    $: console.log(editor)
+    let editor: Monaco.editor.IStandaloneCodeEditor;
+    let monaco: typeof Monaco;
+    let editorContainer: HTMLElement;
+    let options = {
+        value: [
+            'function x() {',
+            '\tconsole.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        language: 'javascript',
+        theme: 'vs-dark',
+        lineNumbers: 'off',
+        minimap: {
+            enabled: false
+        },
+        automaticLayout: true,
+        renderLineHighlight: 'none',
+        scrollbar: {
+            vertical: 'hidden',
+            horizontal: 'hidden'
+        },
+    };
+
+    onMount(async () => {
+
+        // Remove the next two lines to load the monaco editor from a CDN
+        // see https://www.npmjs.com/package/@monaco-editor/loader#config
+        // const monacoEditor = await import('monaco-editor');
+        // loader.config({ monaco: monacoEditor.default });
+
+        monaco = await loader.init();
+
+        // Your monaco instance is ready, let's display some code!
+        const editor = monaco.editor.create(editorContainer, options);
+        const model = monaco.editor.createModel(
+            "console.log('Hello from Monaco! (the editor, not the city...)')",
+            undefined,
+            // Give monaco a hint which syntax highlighting to use
+            monaco.Uri.file('sample.js')
+        );
+        editor.setModel(model);
+    });
+
+    onDestroy(() => {
+        monaco?.editor.getModels().forEach((model) => model.dispose());
+    });
 </script>
 
-<CodeMirror bind:editor {options} bind:value={code}/>
+<svelte:window on:resize={() => editor?.layout({})} />
+
+<div>
+    <div class="container" bind:this={editorContainer} />
+</div>
+
+<style>
+    .container {
+        width: 100%;
+        height: 75vh!important;
+    }
+</style>
