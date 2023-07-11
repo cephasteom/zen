8a3e1201565c3376c1d617962b6895d535ee5b6b
petethomasmusique <petethomasmusique@gmail.com>
2023-07-11 20:14:03 +0100

    content layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,11 @@
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About Zen" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About page</h1>
-</div>
+<section class="content">
+	<h1>This is an H1</h1>
+    <h2>This is an H2</h2>
+    <h3>This is an H3</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</section>
