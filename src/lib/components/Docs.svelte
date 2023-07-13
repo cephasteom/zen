53b5272aed5000b5ea8a0f8324382457baf10c3a
petethomasmusique <petethomasmusique@gmail.com>
2023-07-13 15:52:45 +0100

    make docs more readable

--- src/lib/components/Docs.svelte
+++ src/lib/components/Docs.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
     import { parseTSDocsText } from "$lib/utils/parsing";
     export let json: any;
+    let selectedSection: string;
+    let selectedChild: string;
 </script>
 
 <div>
     <div class="quick-links">
         {#each json.children as section}
-            <span class="quick-link"><a href="/docs#{section.name.toLowerCase()}">{section.name}</a></span>
+            <button 
+                class="quick-link {selectedSection === section.name && 'active'}" 
+                on:click={() => selectedSection = section.name}>
+                    {section.name}
+            </button>
         {/each}
     </div>
     {#each json.children as section}
-        <div class="class">
+        <div class="class {section.name !== selectedSection && 'hidden'}">
             <h3 id={section.name.toLowerCase()}>{section.name}</h3>
             <p class="path">Defined in <a href={section.sources[0]?.url} target="_blank">{section.sources[0]?.fileName}</a></p>
 
@@ -26,60 +32,67 @@
                     {#if child.children}
                         <div class="quick-links">
                             {#each child.children as link}
-                                <span class="quick-link"><a href="/docs#{link.name.toLowerCase()}">{link.name}</a></span>
+                            <button 
+                                class="quick-link {selectedChild === link.name && 'active'}" 
+                                on:click={() => selectedChild = link.name}>
+                                    {link.name}
+                                </button>
                             {/each}
                         </div>
+
                         {#each child.children as subChild}
-                            <h4 id={subChild.name.toLowerCase()}>{subChild.name}</h4>
-                            <p class="path">Defined in <a href={subChild.sources[0]?.url} target="_blank">{subChild.sources[0]?.fileName}</a></p>
-                            {#if subChild.comment}
-                                <p>{@html parseTSDocsText(subChild.comment.summary)}</p>
-                                {#if subChild.comment.blockTags}
-                                    {#each subChild.comment.blockTags as tag}
-                                        <span>{@html parseTSDocsText(tag.content)}</span>
-                                    {/each}
-                                {/if}
-                            {/if}
-
-                            {#if subChild.getSignature}
-                                <p>{@html parseTSDocsText(subChild.getSignature.comment.summary)}</p>
-                                {#if subChild.getSignature.comment.blockTags}
-                                    {#each subChild.getSignature.comment.blockTags as tag}
-                                        <span>{@html parseTSDocsText(tag.content)}</span>
-                                    {/each}
-                                {/if}
-                            {/if}
-
-                            {#if subChild.setSignature}
-                                <p>{@html parseTSDocsText(subChild.setSignature.comment.summary)}</p>
-                                {#if subChild.setSignature.comment.blockTags}
-                                    {#each subChild.setSignature.comment.blockTags as tag}
-                                        <span>{@html parseTSDocsText(tag.content)}</span>
-                                    {/each}
-                                {/if}
-                            {/if}
-
-                            {#if subChild.signatures}
-                                {#each subChild.signatures as signature}
-                                    <p>{@html parseTSDocsText(signature.comment.summary)}</p>
-                                    {#if signature.comment.blockTags}
-                                        {#each signature.comment.blockTags as tag}
+                            <div class="class {subChild.name !== selectedChild && 'hidden'}">
+                                <h4 id={subChild.name.toLowerCase()}>{subChild.name}</h4>
+                                <p class="path">Defined in <a href={subChild.sources[0]?.url} target="_blank">{subChild.sources[0]?.fileName}</a></p>
+                                {#if subChild.comment}
+                                    <p>{@html parseTSDocsText(subChild.comment.summary)}</p>
+                                    {#if subChild.comment.blockTags}
+                                        {#each subChild.comment.blockTags as tag}
                                             <span>{@html parseTSDocsText(tag.content)}</span>
                                         {/each}
                                     {/if}
+                                {/if}
 
-                                    {#if signature.parameters}
-                                        <ul>
-                                            {#each signature.parameters as parameter}
-                                                <li>
-                                                    {#if parameter.name}{parameter.name}{/if}: 
-                                                    {#if parameter.comment}{@html parseTSDocsText(parameter.comment.summary)}{/if}
-                                                </li>
-                                            {/each}
-                                        </ul>
+                                {#if subChild.getSignature}
+                                    <p>{@html parseTSDocsText(subChild.getSignature.comment.summary)}</p>
+                                    {#if subChild.getSignature.comment.blockTags}
+                                        {#each subChild.getSignature.comment.blockTags as tag}
+                                            <span>{@html parseTSDocsText(tag.content)}</span>
+                                        {/each}
                                     {/if}
-                                {/each}
-                            {/if}
+                                {/if}
+
+                                {#if subChild.setSignature}
+                                    <p>{@html parseTSDocsText(subChild.setSignature.comment.summary)}</p>
+                                    {#if subChild.setSignature.comment.blockTags}
+                                        {#each subChild.setSignature.comment.blockTags as tag}
+                                            <span>{@html parseTSDocsText(tag.content)}</span>
+                                        {/each}
+                                    {/if}
+                                {/if}
+
+                                {#if subChild.signatures}
+                                    {#each subChild.signatures as signature}
+                                        <p>{@html parseTSDocsText(signature.comment.summary)}</p>
+                                        {#if signature.comment.blockTags}
+                                            {#each signature.comment.blockTags as tag}
+                                                <span>{@html parseTSDocsText(tag.content)}</span>
+                                            {/each}
+                                        {/if}
+
+                                        {#if signature.parameters}
+                                            <ul>
+                                                {#each signature.parameters as parameter}
+                                                    <li>
+                                                        {#if parameter.name}<strong>{parameter.name}</strong>{/if}: 
+                                                        {#if parameter.comment}{@html parseTSDocsText(parameter.comment.summary)}{/if}
+                                                    </li>
+                                                {/each}
+                                            </ul>
+                                        {/if}
+                                    {/each}
+                                {/if}
+                            </div>
                         {/each}
                     {/if}
                 {/each}
@@ -101,12 +114,27 @@
         margin-top: 1rem;
     }
 
+    ul {
+        padding-left: 0;
+    }
     li {
         margin-top: 0.5rem;
     }
 
+    strong {
+        font-family: 'Lato Bold';
+    }
+
     .class {
         margin-bottom: 4rem;
+    }
+
+    .hidden {
+        display: none;
+    }
+
+    .active {
+        color: var(--color-yellow) !important;
     }
 
     .path {
@@ -121,6 +149,10 @@
     }
     .quick-link {
         margin-right: 1rem;
+        background-color: transparent;
+        border: none;
+        color: var(--color-theme-1);
+        cursor: pointer;
         &:hover {
             text-decoration: underline;
             color: var(--color-yellow)
