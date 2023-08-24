78912ff2515d563c5f040fe83d11679cdde52cb0
petethomasmusique <petethomasmusique@gmail.com>
2023-08-24 22:16:34 +0100

    view docs when in editor mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import Data from '$lib/components/Data.svelte';
     import Tools from '$lib/components/Tools.svelte';
     import { startAudio } from '$lib/zen/index';
+    import { showDocs } from '$lib/stores/app';
 </script>
 
 <svelte:head>
@@ -29,6 +30,9 @@
     </div>
     <div class="data">
         <Data />
+    </div>
+    <div class="docs" style:display={$showDocs ? 'flex' : 'none'}>
+        <iframe title="docs" src="docs/?content=true" frameborder="0"></iframe>
     </div>
 </section>
 
@@ -72,7 +76,7 @@
         padding: 1rem;
     }
 
-    .visuals {
+    .visuals, .docs {
         display: none;
         @media (min-width: 600px) {
             grid-column: 2;
@@ -94,5 +98,14 @@
         }
         background-color: var(--color-grey-dark);
         padding: 1rem;
+    }
+
+    .docs {
+        display: none;
+        & iframe {
+            width: 100%;
+            height: 100%;
+        }
+        z-index: 1000;
     }
 </style>
