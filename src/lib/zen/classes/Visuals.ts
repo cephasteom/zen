0febb05df2798ffe16bee138643be89a6bb41513
petethomasmusique <petethomasmusique@gmail.com>
2023-09-19 09:30:36 +0100

    develop: add interpolate function

--- src/lib/zen/classes/Visuals.ts
+++ src/lib/zen/classes/Visuals.ts
@@ -10,6 +10,7 @@ export class Visuals {
         [0, 163, 153],
     ]
     mColour = [51, 51, 51, 255];
+    background = [38, 38, 38, 255];
     
     constructor(s: number = 16) {
         this.resize(s);
@@ -24,10 +25,10 @@ export class Visuals {
 
     clear() {
         for (let i = 0; i < this._s * this._s; i++) {
-            this.data[i * 4 + 0] = 38;
-            this.data[i * 4 + 1] = 38;
-            this.data[i * 4 + 2] = 38;
-            this.data[i * 4 + 3] = 255;
+            this.data[i * 4 + 0] = this.background[0];
+            this.data[i * 4 + 1] = this.background[1];
+            this.data[i * 4 + 2] = this.background[2];
+            this.data[i * 4 + 3] = this.background[3];
         }
     }
 
@@ -55,8 +56,19 @@ export class Visuals {
         }
     }
 
+    interpolate() {
+        // iterate over data and interpolate between current colour and background
+        for (let i = 0; i < this.data.length; i++) {
+            const c = this.data[i];
+            if(c === this.background[i%4]) continue;
+            this.data[i] = Math.abs(c - this.background[i%4]) > 32
+                ? c > this.background[i%4] ? c - 32 : c + 32
+                : this.background[i%4];
+        }
+    }
+
     get(events: position[], mutations: position[]): Uint8Array {
-        this.clear();
+        this.interpolate();
         this.streams(events, true);
         this.streams(mutations, false);
 
