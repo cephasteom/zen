eb0b41c62d8e7aa8b33b288ba18765bdc5f8c6d9
petethomasmusique <petethomasmusique@gmail.com>
2023-09-27 22:55:27 +0100

    init reverb and delay only when they are used

--- src/lib/oto/classes/Channel.ts
+++ src/lib/oto/classes/Channel.ts
@@ -1,28 +1,51 @@
 import type { Dictionary } from "../types";
 import { Split, context } from 'tone'
-import { CtFXChain } from "../ct-synths"
+import { CtFXChannel, CtFXReverb, CtFXDelay } from "../ct-synths"
 class Channel {
     _fx
     _reverb
+    _delay
     input
+    _output
     constructor(destination: any, channel: number) {
-        const split = new Split({context, channels: 2})
-        split.connect(destination, 0, channel)
-        split.connect(destination, 1, channel+1)
+        this._output = new Split({context, channels: 2})
+        this._output.connect(destination, 0, channel)
+        this._output.connect(destination, 1, channel+1)
         
-        this._fx = new CtFXChain()
+        this._fx = new CtFXChannel()
         
         // routing
         this.input = this._fx.input
-        this._fx.connect(split)
+        this._fx.connect(this._output)
     }
 
     set(params: Dictionary, time: number) {
+        params.reverb > 0 && !this._reverb && this.initReverb()
+        params.delay > 0 && !this._delay && this.initDelay()
         this._fx.set(params, time)
+        this._reverb && this._reverb.set(params, time)
+        this._delay && this._delay.set(params, time)
     }
 
     mutate(params: Dictionary, time: number, lag: number) {
         this._fx.mutate(params, time, lag)
+    }
+
+    initDelay() {
+        console.log('init delay')
+        this._delay = new CtFXDelay()
+        this._fx.connect(this._delay.input)
+        this._reverb
+            ? this._delay.connect(this._reverb.input)
+            : this._delay.connect(this._output)
+    }
+
+    initReverb() {
+        this._reverb = new CtFXReverb()
+        this._reverb.connect(this._output)
+        this._delay 
+            ? this._delay.connect(this._reverb.input)
+            : this._fx.connect(this._reverb.input)
     }
 }
 
