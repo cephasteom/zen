401acfa190701fdb1e64dcff21e23184e08226a7
petethomasmusique <petethomasmusique@gmail.com>
2023-10-02 20:33:55 +0100

    fix logo in safari

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -60,7 +60,6 @@
         color: white;
         font-size: var(--text-sm);
         img {
-            width: 100%;
             height: 100%;
             object-fit: contain;
             filter: invert(1);
