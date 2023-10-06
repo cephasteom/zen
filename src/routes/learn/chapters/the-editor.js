255a23b52a75588b7d760b14be93dd280f6c7add
petethomasmusique <petethomasmusique@gmail.com>
2023-10-06 09:21:05 +0100

    fix typo

--- src/routes/learn/chapters/the-editor.js
+++ src/routes/learn/chapters/the-editor.js
@@ -1,7 +1,7 @@
 export default `# The Editor
 The best place to start is by using the <a href="https://zen.cephasteom.co.uk" target="_blank">online editor</a>. On the left, you should see a code editor and on the right, a console for communicating errors and debugging your code.
 
-In the toolbar, their are controls for play/stop, save and load. The final icon toggles between the console and a pattern visualiser. We recommend turning on the visualiser whilst learning Zen. Below the console is displayed the current time \`t\`, the current cycle \`c\`, the number of divisions per cycle \`q\` and the size of the visualiser canvas \`s\`. More on these values later.
+In the toolbar, there are controls for play/stop, save and load. The final icon toggles between the console and a pattern visualiser. We recommend turning on the visualiser whilst learning Zen. Below the console is displayed the current time \`t\`, the current cycle \`c\`, the number of divisions per cycle \`q\` and the size of the visualiser canvas \`s\`. More on these values later.
 
 Editor commands:
 - \`shift+enter\` executes the code and starts playback
