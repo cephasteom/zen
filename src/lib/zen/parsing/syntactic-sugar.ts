94ab0cec95b469c93d1546b46b643f1b47659bc4
petethomasmusique <petethomasmusique@gmail.com>
2023-12-04 14:17:39 +0000

    fix parsing errors

--- src/lib/zen/parsing/syntactic-sugar.ts
+++ src/lib/zen/parsing/syntactic-sugar.ts
@@ -3,10 +3,10 @@ import { pipe } from '../utils/utils'
 export const parseCode = (code: string) => {
     return pipe(
         // replace p.banana() with p.banana.set()
-        (code: string) => code.replace(/p\.(\w+)\(/g, 'p.$1.set('),
-        (code: string) => code.replace(/px\.(\w+)\(/g, 'px.$1.set('),
-        (code: string) => code.replace(/py\.(\w+)/g, 'py.$1.set('),
-        (code: string) => code.replace(/pz\.(\w+)/g, 'pz.$1.set('),
+        (code: string) => code.replace(/\.p\.(\w+)\(/g, '.p.$1.set('),
+        (code: string) => code.replace(/\.px\.(\w+)\(/g, '.px.$1.set('),
+        (code: string) => code.replace(/\.py\.(\w+)/g, '.py.$1.set('),
+        (code: string) => code.replace(/\.pz\.(\w+)/g, '.pz.$1.set('),
         // replace .e() with .e.set()
         (code: string) => code.replace(/\.e\(/g, '.e.set('),
         // replace .m() with .m.set()
