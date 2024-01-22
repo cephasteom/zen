16006305da1aa3ce3321ac2dd77cb1a7c56429fe
petethomasmusique <petethomasmusique@gmail.com>
2024-01-22 14:45:42 +0000

    develop: improve midi docs

--- src/routes/learn/chapters/midi.js
+++ src/routes/learn/chapters/midi.js
@@ -1,7 +1,9 @@
 export default `# MIDI
-Midi is simple to use in Zen. On page load, all available midi inputs and outputs are printed in your console for reference. The index of the device you wish to use should be assigned to a stream's \`midi\` parameter. The following parameters control midi routing in Zen:
+Midi is simple to use in Zen. On page load, all available midi inputs and outputs are printed in your console for reference. The index of the device you wish to use should be assigned to a stream's \`midi\` parameter.
 
 ## MIDI Output
+The following parameters control midi routing in Zen:
+
 - \`midi\` the midi device to send messages to (passed as an index).
 - \`midichan\` the midi channel to send messages to. Sends to all channels if not included.
 - \`mididelay\` delay midi messages by n milliseconds. Useful for synchronising midi and audio.
