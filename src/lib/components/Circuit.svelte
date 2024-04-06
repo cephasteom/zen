8c3fa233045a283a98f3afb8646df397efd25f1d
petethomasmusique <petethomasmusique@gmail.com>
2024-04-06 20:22:41 +0100

    epic/zenq: basic circuit layout

--- src/lib/components/Circuit.svelte
+++ src/lib/components/Circuit.svelte
@@ -1,19 +1,36 @@
 <script lang="ts">
     let rows = [0,1,2,3,4,5,6,7]
-    let cols = [0,1,2,3,4,5,6,7]
+    let cols = [0,1,2,3,4,5,6]
 </script>
 
 <div class="circuit">
-    {#each rows as row}
+    {#each rows as row, i}
         <div class="row">
+            <div 
+                    class="col"
+                    style="grid-column: 1;"
+            >
+                <p class="label">
+                    <span class="label__state">|0⟩</span>
+                    <span class="label__stream">s{i}</span>
+                </p>
+            </div>
             {#each cols as col, i}
                 <div 
                     class="col"
-                    style="grid-column: {i + 1};"
+                    style="grid-column: {i + 2};"
                 >
                     <span class="wire"></span>
                 </div>
             {/each}
+            <div 
+                    class="col"
+                    style="grid-column: 9;"
+            >
+                <p class="label">
+                    <span class="label__output">|0⟩</span>
+                </p>
+            </div>
         </div>
     {/each}
 </div>
@@ -23,16 +40,15 @@
         display: grid;
         grid-template-columns: 1fr;
         grid-template-rows: repeat(8, 1fr);
-        background-color: #333;
-        border: 1px solid #333;
         width: 100%;
-        height: 100%;
+        height: calc(100% - 2rem);
         border-radius: 10px;
+        padding: 2rem;
     }
 
     .row {
         display: grid;
-        grid-template-columns: repeat(8, 1fr);
+        grid-template-columns: 0.25fr repeat(7, 1fr) 0.25fr;
         grid-template-rows: 1fr;
     }
 
@@ -41,9 +57,26 @@
         align-items: center;
     }
 
+    .label {
+        margin: 0;
+        display: flex;
+        position: relative;
+        color: var(--color-theme-1);
+        &__stream {
+            font-size: 0.75rem;
+            position: absolute;
+            right: -1.25rem;
+            top: -0.5rem;
+        }
+
+        &__output {
+            margin-left: 0.3rem;
+        }
+    }
+
     .wire {
         width: 100%;
-        height: 2px;
+        height: 1px;
         background-color: var(--color-theme-1);
     }
 </style>
