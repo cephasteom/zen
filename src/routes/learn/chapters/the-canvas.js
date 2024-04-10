3bfd143df9e3844c742b3ef207ca301fefd8d969
petethomasmusique <petethomasmusique@gmail.com>
2024-04-10 22:02:45 +0100

    epic/zenq: scale x y and z so that wemcan use normalised values

--- src/routes/learn/chapters/the-canvas.js
+++ src/routes/learn/chapters/the-canvas.js
@@ -2,7 +2,7 @@ export default `# The Canvas
 So far we’ve mapped all parameters across time using the \`.p\` property. Zen also maps parameters across space, allowing you to compose in 4 dimensions.
 
 ## .x .y .z
-The \`.x\`, \`.y\`, and \`.z\` properties of a stream allow you to move a stream around a virtual space and are all instances of the [Pattern class](/docs/classes#pattern). This movement is represented in 3 dimensions (xyz) on the visualiser using the concept of a sphere.
+The \`.x\`, \`.y\`, and \`.z\` properties of a stream allow you to move a stream around a virtual space and are all instances of the [Pattern class](/docs/classes#pattern). This movement is represented in 3 dimensions (xyz) on the visualiser using the concept of a sphere. We have chosen to use the x axis as the horizontal rotation of the sphere, the y axis as the vertical rotation, and the z axis as the distance from the centre of the sphere.
 \`\`\`js
 s0.e.every(1)
 s0.x.saw(0,16)
@@ -25,4 +25,6 @@ s0.e.every(1)
 \`\`\`
 
 The canvas can be used or ignored at will. If you don’t need to map parameters across space, you can use the \`.p\` property as normal. However, composing with four dimensions can be a useful way of managing complexity. For example, you could place the overall intensity of the piece on the z axis, and use the x and y axes to control the movement of individual streams.
+
+As we shall see, plotting a stream's position in 3D space comes into its own when you start using Zen's quantum features.
 `
