4022f608be9a37aab977d7f5e721ec9b4dc1848f
petethomasmusique <petethomasmusique@gmail.com>
2024-04-11 21:47:32 +0100

    epic/zenq: docs

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,27 +4,39 @@
 </svelte:head>
 
 <section class="content">
-	<h1>Live code in your browser</h1>
-    <h2>About</h2>
-    <p>Zen is a live coding environment for expressing complex patterns with very little code. It was written to take advantage of JavaScript's flexibility and is primed for pattern interference. Design and run quantum circuits and use the results to trigger musical events. Map musical and sonic parameters across time and space and draw multiple trajectories around a canvas to generate infinitely complex, morphing sounds.</p>
+    <div class="content__inner">
+        <h1>Live code in your browser</h1>
+        <h2>About</h2>
+        <p>Zen is a live coding environment for expressing complex patterns with very little code. It was written to take advantage of JavaScript's flexibility and is primed for pattern interference. Design and run quantum circuits and use the results to trigger musical events. Map musical and sonic parameters across time and space and draw multiple trajectories around a canvas to generate infinitely complex, morphing sounds.</p>
 
-    <h3>Credits</h3>
-    <p>Zen is the work of Cephas Teom, a musician and coder based in the UK. You can see more of his work by following the links below:</p>
-    <ul>
-        <li><a href="https://cephasteom.co.uk/" target="_blank">Website</a></li>
-        <li><a href="https://cephasteom.bandcamp.com/album/automata" target="_blank">Bandcamp</a></li>
-        <li><a href="https://metrmusic.com/" target="_blank">Label</a></li>
-    </ul>
-    <p>Zen is built on Svelte and is indebted to the following technologies:</p>
-    <ul>
-        <li><a href="https://tonejs.github.io/" target="_blank">Tone.js</a></li>
-        <li><a href="https://rnbo.cycling74.com/" target="_blank">RNBO (Cycling '74')</a></li>
-        <li><a href="https://www.npmjs.com/package/quantum-circuit" target="_blank">Quantum Circuit (Quantastica)</a></li>
-    </ul>
-
+        <h3>Credits</h3>
+        <p>Zen is the work of Cephas Teom, a musician and coder based in the UK. You can see more of his work by following the links below:</p>
+        <ul>
+            <li><a href="https://cephasteom.co.uk/" target="_blank">Website</a></li>
+            <li><a href="https://cephasteom.bandcamp.com/album/automata" target="_blank">Bandcamp</a></li>
+            <li><a href="https://metrmusic.com/" target="_blank">Label</a></li>
+            <li><a href="https://lunar.build/" target="_blank">Lunar</a></li>
+        </ul>
+        <p>Zen is built on Svelte and is indebted to the following technologies:</p>
+        <ul>
+            <li><a href="https://tonejs.github.io/" target="_blank">Tone.js</a></li>
+            <li><a href="https://rnbo.cycling74.com/" target="_blank">RNBO (Cycling '74')</a></li>
+            <li><a href="https://www.npmjs.com/package/quantum-circuit" target="_blank">Quantum Circuit (Quantastica)</a></li>
+        </ul>
+    </div>
 </section>
 
 <style lang="scss">
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        &__inner {
+            width: 100%;
+            max-width: 1200px;
+            margin: 0 auto 2rem;
+        }
+    }
     ul {
         padding-left: 0;
     }
