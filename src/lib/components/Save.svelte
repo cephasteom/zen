902675e4055d08c8c9a1970e9fa617a777ae7f5c
Peter Thomas <petethomasmusique@gmail.com>
2024-06-12 11:06:11 +0100

    feature/save: show preset keys in save module

--- src/lib/components/Save.svelte
+++ src/lib/components/Save.svelte
@@ -3,20 +3,83 @@
     const dispatch = createEventDispatcher();
 
     import Input from './Input.svelte'
-    import { savePreset } from '$lib/stores/presets'
+    import { savePreset, presetKeys, activePreset } from '$lib/stores/presets'
+
+    function handleKeydown(e: KeyboardEvent) {
+        switch (e.key) {
+            case 'ArrowDown':
+                index = Math.min(index + 1, $presetKeys.length - 1)
+                break;
+            case 'ArrowUp':
+                index = Math.max(index - 1, -1)
+                break;
+        }
+    }
+
+    $: index = $presetKeys.indexOf($activePreset)
+    $: start = index < 5 ? 0 : index - 4
+    $: end = start + 5
+    $: visiblePresets = $presetKeys.slice(start, end)
 </script>
 
 
-<!-- svelte-ignore a11y-label-has-associated-control -->
-<label>Save as</label>
-<Input id="save" placeholder="name" on:enter={({detail}) => {
-    dispatch('save')
-    savePreset(detail) 
-}} />
+<div on:keydown={handleKeydown}>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>Save as</label>
+    <Input 
+        id="save" 
+        placeholder="name" 
+        value={$presetKeys[index] || ''} 
+        on:enter={({detail}) => {
+            dispatch('save')
+            savePreset(detail)
+        }}
+    />
+    <ul>
+        {#each visiblePresets as key}
+            <li 
+                class:active={key === $presetKeys[index]}
+            ><button 
+                on:click={() => {
+                    dispatch('save')
+                    savePreset(key) 
+                }}
+            >{key}</button></li>
+        {/each}
+    </ul>
+</div>
 
 <style lang="scss">
     label {
         padding-right: 0.125rem;
         color: var(--color-grey-light);
+    }
+
+    ul {
+        list-style: none;
+        padding-left: 0;
+    }
+
+    li {
+        padding: 0.5rem 0.5rem;
+        margin: 0;
+        margin-left: 69.5px;
+
+        &:hover, &.active {
+            background-color: var(--color-yellow);
+            & button { color: var(--color-grey-dark) }
+        }
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+        color: var(--color-yellow);
+        font-family: var(--font-family);
+        padding: 0;
+        text-align: left;
+        width: 100%;
+        height: 100%;
     }
 </style>
--- src/lib/stores/presets.ts
+++ src/lib/stores/presets.ts
@@ -23,11 +23,13 @@ export const presetKeys = derived(
 )
 
 export function savePreset(key: string) {
+    console.log(key)
     const stored = JSON.parse(localStorage.getItem('z.presets') || "{}");
     stored[key] = get(editorValue) // get current code from the Editor
     
     localStorage.setItem('z.presets', JSON.stringify(stored));
     presets.update(presets => ({...presets, ...stored}))
+    activePreset.set(key)
 }
 
 export function deletePreset(key: string) {
