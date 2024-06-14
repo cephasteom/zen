f8c0bfd73f8ad15e203c45e5138818337bf5c150
Peter Thomas <petethomasmusique@gmail.com>
2024-06-14 22:17:39 +0200

    bug/circuit: use in built svg, but with same issue

--- src/lib/components/Circuit.svelte
+++ src/lib/components/Circuit.svelte
@@ -1,151 +1,45 @@
 <script lang="ts">
-    import { gates, measurements, inputs } from "$lib/stores/zen";
-    import Gate from "./Gate.svelte";
+    import { onMount } from 'svelte';
+    import { t } from '$lib/stores/zen'
+    import { circuit } from '$lib/zen/classes/Circuit';
+    
+    let thisSvg: HTMLDivElement | null = null;
+    let svg: string = "";
 
-    const getConnectedRow = (currentRow: number, currentCol: number) => {
-        const { name, connector } = $gates[currentRow][currentCol];
-        for (let row = 0; row < $gates.length; row++) {
-            if (row !== currentRow) {
-                const gate = $gates[row][currentCol];
-                if (gate && gate.name === name && connector - gate.connector === 1 ) {
-                    return row;
-                }
-            }
-        }
-        return null;
-    };
+    onMount(() => {
+        t.subscribe((value) => {
+            svg = circuit.exportSVG(true)
+        });
+    });
 </script>
 
 <div 
     class="circuit"
-    style={`justify-content: ${$gates.length === 6 ? 'flex-start' : 'center'};`}
 >
-    <div class="circuit__wires">
-        {#each $gates as gates, row}
-            <div class="row">
-                <div class="col col--first">
-                    <p class="label">
-                        <span class="label__state">|{$inputs[row]}⟩</span>
-                        <span class="label__stream">s{row}</span>
-                    </p>
-                </div>
-                <div class="gates">
-                    <span class="wire"/>
-
-                    {#each gates as gate, col}
-                        <div 
-                            class="col"
-                        >
-                            {#if gate}
-                                <Gate
-                                    gate={gate}
-                                    row={row}
-                                    connectTo={getConnectedRow(row, col)}
-                                />
-                            {/if}
-                        </div>
-                    {/each}
-                </div>
-                <div 
-                    class="col col--last"
-                >
-                    <p class="label">
-                        <span class="label__output">|{$measurements[row]}⟩</span>
-                    </p>
-                </div>
-            </div>
-        {/each}
-    </div>
+    {#if svg}
+        <div 
+            bind:this={thisSvg}
+            class="circuit-svg"
+        >
+            {@html svg}
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
     .circuit {
         width: calc(100% - 4rem);
-        height: calc(100% - 3rem);
+        height: 100%;
         border-radius: 10px;
-        padding: 1rem 2rem 2rem;
-        overflow: hidden;
+        padding: 0rem 2rem;
+        overflow: scroll;
         position: relative;
-
-        &::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 5rem;
-            pointer-events: none;
-            z-index:10;
+        span {
+            
         }
 
-        &__wires {
-            width: 100%;
-            height: 100%;
-            overflow-x: scroll;
-        }
     }
 
-    .row {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-        min-height: 3rem;
-    }
-
-    .col {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        position: relative;
-        width: 3rem;
-        &--first {
-            justify-content: flex-start;
-        }
-
-        &--last {
-            justify-content: flex-end;
-        }
-    }
-
-    .label {
-        margin: 0;
-        display: flex;
-        position: relative;
-        color: var(--color-theme-2);
-        font-size: var(--text-sm);
-        font-family: 'Lato Bold';
-        &__stream {
-            font-size: var(--text-xxs);
-
-            position: absolute;
-            right: -0.5rem;
-            top: -0.5rem;
-        }
-
-        &__output {
-            margin-left: 0.3rem;
-        }
-    }
-
-
-    .gates {
-        display: flex;
-        width: 100%;
-        overflow-x: scroll;
-        position: relative;
-        overflow: visible;
-        & .col:first-of-type {
-            margin-left: 1rem;
-        }
-    }
-
-    .wire {
-        width: 100%;
-        height: 2px;
-        background-color: var(--color-theme-2);
-        position: absolute;
-        top: calc(50% - 0.5px);
-        z-index: 10;
-    }
+    
 </style>
 
