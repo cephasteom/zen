7716ca5c490e7026f50d62339de4ec69dec58560
Peter Thomas <petethomasmusique@gmail.com>
2024-09-26 22:42:01 +0100

    refactor/circuit: use svg rather than inbuilt

--- src/lib/components/Circuit.svelte
+++ src/lib/components/Circuit.svelte
@@ -1,28 +1,36 @@
 <script lang="ts">
     import { gates, measurements, inputs } from "$lib/stores/zen";
-    import Gate from "./Gate.svelte";
+    import { circuit } from '$lib/zen/classes/Circuit'
+    import { code } from '$lib/zen';
+    import { onMount } from "svelte";
 
-    const getConnectedRow = (currentRow: number, currentCol: number) => {
-        const { id, name, connector } = $gates[currentRow][currentCol];
-        for (let row = 0; row < $gates.length; row++) {
-            if (row !== currentRow) {
-                const gate = $gates[row][currentCol];
-                if (gate && gate.name === name && connector - gate.connector === 1 
-                    && gate.id === id
-                ) {
-                    return row;
-                }
-            }
+    let svg = '';
+    let thisSvg: HTMLDivElement;
+
+    onMount(() => {
+        const unsubscribeCode = code.subscribe(() => {
+            svg = circuit.gates.flat().length > 0
+                ? circuit.exportSVG()
+                : '';
+        });
+
+        return () => {
+            unsubscribeCode();
         }
-        return null;
-    };
+    });
+
 </script>
 
 <div 
     class="circuit"
-    style={`justify-content: ${$gates.length === 6 ? 'flex-start' : 'center'};`}
 >
-    <div class="circuit__wires">
+    <div 
+        bind:this={thisSvg}
+        class="circuit__svg"
+    >
+        {@html svg}
+    </div>
+    <!-- <div class="circuit__wires">
         {#each $gates as gates, row}
             <div class="row">
                 <div class="col col--first">
@@ -31,23 +39,7 @@
                         <span class="label__stream">s{row}</span>
                     </p>
                 </div>
-                <div class="gates">
-                    <span class="wire"/>
 
-                    {#each gates as gate, col}
-                        <div 
-                            class="col"
-                        >
-                            {#if gate}
-                                <Gate
-                                    gate={gate}
-                                    row={row}
-                                    connectTo={getConnectedRow(row, col)}
-                                />
-                            {/if}
-                        </div>
-                    {/each}
-                </div>
                 <div 
                     class="col col--last"
                 >
@@ -57,7 +49,7 @@
                 </div>
             </div>
         {/each}
-    </div>
+    </div> -->
 </div>
 
 <style lang="scss">
@@ -66,7 +58,7 @@
         height: calc(100% - 3rem);
         border-radius: 10px;
         padding: 1rem 2rem 2rem;
-        overflow: hidden;
+        overflow: auto; /* Enable scrolling */
         position: relative;
 
         &::after {
@@ -80,74 +72,85 @@
             z-index:10;
         }
 
-        &__wires {
-            width: 100%;
-            height: 100%;
-            overflow-x: scroll;
-        }
-    }
-
-    .row {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-        min-height: 3rem;
-    }
-
-    .col {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        position: relative;
-        width: 3rem;
-        &--first {
-            justify-content: flex-start;
+        &__svg {
+            max-width: calc(50vw - 4rem);
+            max-height: 100%;
+            display: bloxk;
+            overflow: scroll;
+            
+            @media (min-width: 1200px) {
+                max-width: calc(50vw - 6rem);
+            }
         }
 
-        &--last {
-            justify-content: flex-end;
-        }
+        // &__wires {
+        //     width: 100%;
+        //     height: 100%;
+        //     overflow-x: scroll;
+        // }
     }
 
-    .label {
-        margin: 0;
-        display: flex;
-        position: relative;
-        color: var(--color-theme-2);
-        font-size: var(--text-sm);
-        font-family: 'Lato Bold';
-        &__stream {
-            font-size: var(--text-xxs);
+    // .row {
+    //     display: flex;
+    //     justify-content: space-between;
+    //     width: 100%;
+    //     min-height: 3rem;
+    // }
 
-            position: absolute;
-            right: -0.5rem;
-            top: -0.5rem;
-        }
+    // .col {
+    //     display: flex;
+    //     align-items: center;
+    //     justify-content: center;
+    //     position: relative;
+    //     width: 3rem;
+    //     &--first {
+    //         justify-content: flex-start;
+    //     }
 
-        &__output {
-            margin-left: 0.3rem;
-        }
-    }
+    //     &--last {
+    //         justify-content: flex-end;
+    //     }
+    // }
+
+    // .label {
+    //     margin: 0;
+    //     display: flex;
+    //     position: relative;
+    //     color: var(--color-theme-2);
+    //     font-size: var(--text-sm);
+    //     font-family: 'Lato Bold';
+    //     &__stream {
+    //         font-size: var(--text-xxs);
+
+    //         position: absolute;
+    //         right: -0.5rem;
+    //         top: -0.5rem;
+    //     }
+
+    //     &__output {
+    //         margin-left: 0.3rem;
+    //     }
+    // }
 
 
-    .gates {
-        display: flex;
-        width: 100%;
-        overflow-x: scroll;
-        position: relative;
-        overflow: visible;
-        & .col:first-of-type {
-            margin-left: 1rem;
-        }
-    }
+    // .gates {
+    //     display: flex;
+    //     width: 100%;
+    //     overflow-x: scroll;
+    //     position: relative;
+    //     overflow: visible;
+    //     & .col:first-of-type {
+    //         margin-left: 1rem;
+    //     }
+    // }
 
-    .wire {
-        width: 100%;
-        height: 2px;
-        background-color: var(--color-theme-2);
-        position: absolute;
-        top: calc(50% - 0.5px);
-        z-index: 10;
-    }
+    // .wire {
+    //     width: 100%;
+    //     height: 2px;
+    //     background-color: var(--color-theme-2);
+    //     position: absolute;
+    //     top: calc(50% - 0.5px);
+    //     z-index: 10;
+    // }
 </style>
 
