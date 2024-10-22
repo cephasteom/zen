bc7fd2f8b2b0b022ef3e454e3e64cf92c76293fe
Peter Thomas <petethomasmusique@gmail.com>
2024-10-22 08:50:52 +0100

    improve instruments docs

--- src/routes/learn/chapters/instruments.js
+++ src/routes/learn/chapters/instruments.js
@@ -8,13 +8,16 @@ Most instruments share a common set of parameters. Envelope parameters are \`a\`
 An all purpose synth with filters and FM:
 \`\`\`js
 s0.set({inst:'synth',cut:0,re:0.25,rdecay:0.75,de:0.25,lag:ms(2),locut:0.3})
+
+s0.x.saw()
+s0.y.noise()
+
 s0.p.n.set('Dpro%16..?*16|*4').sub(12)
 s0.p.mods.random(0.1,1,0.5)
 s0.p.dur.noise(0.1,2,0.75).btms()
-s0.px._modi.saw(0.25,0.5)
-s0.py._harm.saw(0.5,3,0.25)
-s0.x.saw()
-s0.y.noise()
+s0.p._modi.set(s0.x).saw(0.25,0.5)
+s0.p._harm.set(s0.y).saw(0.5,3,0.25)
+
 s0.e.every('1?2*16')
 s0.m.not(s0.e)
 \`\`\`
@@ -24,13 +27,14 @@ The sampler takes the name of a sample bank and the index of a file within that 
 \`\`\`js
 z.bpm.set(160)
 
-s0.set({inst:1,bank:'breaks',snap:q,dur:ms(1),cut:[0,1,2]})
+s0.set({inst:1,bank:'breaks',snap:16,dur:ms(1),cut:[0,1,2]})
 s0.e.set('1')
 
-s1.set({inst:1,ba:'breaks',snap:q,cut:[0,1],dur:ms(8),loop:1,cutr:ms(0.5),re:0.125,rs:0.1})
-s1.px.begin.v('1|*3 0').if(saw(0,1,1/32,1/16)).else(random(0,0.75,1/16))
-s1.px.i.v('0|*3 1')
-s1.e.not(s0.e).and(every('8|*4 4|*4'))
+s1.set({inst:1,ba:'breaks',snap:16,cut:[0,1],dur:ms(8),loop:1,cutr:ms(0.5),re:0.125,rs:0.1})
+s1.x.saw(0,1,1/4)
+s1.p.begin.set(s1.x).saw().step(1/8)
+s1.p.i.v('0|*3 1')
+s1.e.not(s0.e).and(every(16))
 \`\`\`
 
 ## Granular Synth
