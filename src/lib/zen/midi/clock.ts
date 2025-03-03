b91702b0a4a944837812ac0bcf6697ae0db837d6
Peter Thomas <petethomasmusique@gmail.com>
2025-03-03 22:25:56 +0000

    amend/clock-bpm: remove console statements

--- src/lib/zen/midi/clock.ts
+++ src/lib/zen/midi/clock.ts
@@ -41,7 +41,6 @@ export function initMidiClock() {
                 const ticksPerDivision = Math.floor(ticksPerCycle/q);
 
                 const beats = euclidean(q, ticksPerCycle);
-                const t = 
 
                 beats[tickCount % ticksPerCycle]
                     && evaluate(tickCount / ticksPerDivision, immediate())
