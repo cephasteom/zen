962468291ca5cd27e5c8f07c29980bc86e076395
Peter Thomas <petethomasmusique@gmail.com>
2025-03-25 16:26:30 +0000

    feature/save-load files

--- src/lib/electronAPI/electronAPI.d.ts
+++ src/lib/electronAPI/electronAPI.d.ts
@@ -2,5 +2,7 @@ interface Window {
     isApp: boolean;
     electronAPI: {
         onLoad: (callback: (code: string) => void) => void;
+        onGetCode(callback: () => void): void;
+        getCodeResponse(code: string): void;
     }
 }
--- src/lib/electronAPI/index.ts
+++ src/lib/electronAPI/index.ts
@@ -1,10 +1,15 @@
-import { loadCode } from '$lib/stores/zen';
+import { loadCode, editorValue } from '$lib/stores/zen';
+import { get } from 'svelte/store';
 
 export function initElectronAPI() {
 
     if(typeof document === 'undefined' || (typeof window === 'undefined')) return
     
     window.electronAPI.onLoad((code: string) => loadCode.set(code));
+    window.electronAPI.onGetCode(() => {
+        const code: string = get(editorValue);
+        window.electronAPI.getCodeResponse(code);
+    });
 }
 
 export const isApp: () => boolean = () => {
