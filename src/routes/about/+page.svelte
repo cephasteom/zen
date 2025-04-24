ad6ecb6b7ee2a72157602da248f36109d0a34835
Peter Thomas <petethomasmusique@gmail.com>
2025-04-24 20:48:28 +0100

    link to the iccmr

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,7 +10,9 @@
         <p>Zen is a live coding environment for expressing complex patterns with very little code. It was written to take advantage of JavaScript's flexibility and is primed for pattern interference. Design and run quantum circuits and use the results to trigger musical events. Map musical and sonic parameters across time and space and draw multiple trajectories around a canvas to generate infinitely complex, morphing sounds.</p>
 
         <h3>Credits</h3>
-        <p>Zen is the work of Cephas Teom, a musician and coder based in the UK. You can see more of his work by following the links below:</p>
+        <p>Zen is the work of Cephas Teom, a musician and coder based in the UK. It was devised, researched, and built at the <a href="https://www.plymouth.ac.uk/research/iccmr" target="_blank">ICCMR, University of Plymouth</a>, as part of the <a href="https://www.plymouth.ac.uk/courses/postgraduate/phd-computer-music" target="_blank">PhD Computer Music</a>, exploring the intersection of quantum computer music, live coding, and the web.</p>
+            
+        <p>You can see more of his work by following the links below:</p>
         <ul>
             <li><a href="https://cephasteom.co.uk/" target="_blank">Website</a></li>
             <li><a href="https://cephasteom.bandcamp.com/album/automata" target="_blank">Bandcamp</a></li>
