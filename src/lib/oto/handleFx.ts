3363529046f6ba4c1aa67455488d1d7043ae80fe
Peter Thomas <petethomasmusique@gmail.com>
2025-05-01 12:12:33 +0100

    feature/out: fix out routing for fx streams

--- src/lib/oto/handleFx.ts
+++ src/lib/oto/handleFx.ts
@@ -4,10 +4,9 @@ import { output, fxChannels } from './routing';
 export const handleFxEvent = (time: number, id: string, params: Dictionary) => {
     if(!['fx0', 'fx1', 'fx2', 'fx3'].includes(id)) return
     
-    const { track } = params;
-    const channel = (track || +id.replace('fx', '') || 0) * 2
+    const { out = 0 } = params;
 
-    fxChannels[id].routeOut(channel % output.numberOfInputs)
+    fxChannels[id].routeOut(out)
 
     // set fx params on that channel
     fxChannels[id].set(params, time)
@@ -15,10 +14,9 @@ export const handleFxEvent = (time: number, id: string, params: Dictionary) => {
 
 export const handleFxMutation = (time: number, id: string, params: Dictionary) => {
     if(!['fx0', 'fx1', 'fx2', 'fx3'].includes(id)) return
-    const { track, lag=500 } = params;
-    const channel = (track || +id.replace('fx', '') || 0) * 2
+    const { lag=500, out = 0 } = params;
 
-    fxChannels[id].routeOut(channel % output.numberOfInputs)
+    fxChannels[id].routeOut(out)
 
     // mutate fx params on that channel
     fxChannels[id].mutate(params, time, lag)
