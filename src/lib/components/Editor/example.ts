775c7b2168d9570f7dae23e9490b9155933ee0ad
Peter Thomas <petethomasmusique@gmail.com>
2025-06-24 21:25:44 +0100

    epic/zen-5: increment package

--- src/lib/components/Editor/example.ts
+++ src/lib/components/Editor/example.ts
@@ -1,7 +1,7 @@
-// export const example = `s0.set({inst:0,cut:0,reverb:.5,delay:.25,vol:.5,modi:1.25,mods:0.1})
-// s0.p.n.set('Cpro%16..*16 | Cpro%16..?*16').sub(12)
-// s0.p.s.noise(0.05,0.5,0.25)
-// s0.e.every(4).or(every(3))
-// `
+export const example = `s0.set({inst:0,cut:0,reverb:.5,delay:.25,vol:.5,modi:1.25,mods:0.1})
+s0.p.n.set('Cpro%16..*16 | Cpro%16..?*16').sub(12)
+s0.p.s.noise(0.05,0.5,0.25)
+s0.e.every(4).or(every(3))
+`
 
-export const example = ''
+// export const example = ''
