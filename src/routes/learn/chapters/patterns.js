505dc1b1759962e67d6d4cbe68a202a2445de471
Peter Thomas <petethomasmusique@gmail.com>
2025-10-03 22:43:20 +0100

    epic/zen-5: docs refactor up to mutations

--- src/routes/learn/chapters/patterns.js
+++ src/routes/learn/chapters/patterns.js
@@ -1,20 +1,21 @@
 export default `# Patterns
-The [Pattern class](/docs/classes#pattern) is Zen’s core building block — in Zen, *everything is a pattern*. Mastering patterns means mastering Zen. Run the following code in the editor and note the changing values in the console:
+The [Pattern class](/docs/classes#pattern) is Zen’s core building block — in Zen, *everything is a pattern*. Run this code and see the changing values in the console:
 \`\`\`js
-s0.x.saw().print()
-s0.e.every(1)
+s0.x.saw().print('x')
+s0.e.every(1).print('e')
 \`\`\`
-Here, \`s0\` is a Stream. We'll cover these in the next chapter. Every parameter of a Stream returns a Pattern. \`.e\` is a special parameter that triggers events. Other parameters like \`.x\`, \`.y\`, or even \`.banana\`, are user-defined and can be used for anything you like.
+Here, \`s0\` is a Stream. Every parameter of a Stream returns a Pattern. \`.e\` is a special parameter that triggers events. Other parameters like \`.x\`, \`.y\`, or even \`.banana\`, are user-defined and can be used for anything you like. We'll cover Streams in depth in the [next chapter](/learn/streams). 
 
-Patterns have many useful methods for generating interesting streams of values. The \`.set()\` method is used to set a constant value, whereas \`every()\` returns a 1 every n divisions. For example:
+Patterns have many methods for generating interesting patterns of values. For example, the \`.set()\` method is used to set a constant value, whereas \`every()\` returns a 1 every *n* divisions. For example:
 \`\`\`js
-s0.x.set(0.5).print()
-s0.e.every(4).print()
+s0.x.set(0.5).print('x')
+s0.e.every(4).print('e')
 \`\`\`
 
-Others, like \`.sine()\`, \`.tri()\`, and \`.square()\`, return changing values. Most methods return normalised values (between 0 and 1), but take additional arguments for frequency and scaling:
+Others, like \`.sine()\`, \`.tri()\`, and \`.square()\`, return values that change over time. Most methods generate values between 0 and 1, and take additional arguments for frequency and scaling:
 \`\`\`js
-s0.x.sine(2,0,0.5).print() // a sine wave that oscillates between 0 and 0.5 every 2 cycles
+s0.x.sine(2,0,0.5).print('x') // a sine wave that oscillates between 0 and 0.5 every 2 cycles
+s0.e.set(1) // trigger the stream
 \`\`\`
 
 Pattern methods can be chained:
@@ -30,17 +31,20 @@ s0.y.set(1).sub(s0.x).print('y') // set y to be the inverse of x
 s0.e.set(1)
 \`\`\`
 
-All Pattern methods exist in the global scope, which allows you to nest Patterns in other Patterns. For example:
+You can nest Patterns. For example:
 \`\`\`js
-s0.x.sine(1,saw().mul(noise()),1)
+s0.x.saw(0.25,0,64)
+    .mul(coin().ifelse(1,100))
+    .print('x')
 s0.e.set(1)
 \`\`\`
 
-You can also use a [mini-notation](/learn/mini-notation) string to create patterns. For example:
+You can also use [mini-notation](/learn/mini-notation), which we'll cover in more detail later. For example:
 \`\`\`js
-s0.x.set("0 0.25 0.5 0.75")
-s0.y.saw(4,0,1)
-s0.e.set("1*4 0*4 1*4 0*4")
+s0.x.set("0 0.25 0.5 0.75").print('x')
+s0.y.set("0..8*8").print('y')
+s0.n.set('D4 F4 A4 B4').print('n')
+s0.e.every(4)
 \`\`\`
 
 For a full list of Pattern methods and their arguments, see the [Pattern docs](/docs/classes#pattern).
--- src/routes/learn/chapters/streams.js
+++ src/routes/learn/chapters/streams.js
@@ -1,23 +1,25 @@
 export default `# Streams
-Zen is organised into Streams, which refer to different musical layers. Streams are represented by the letter \`s\` and an index, as in \`s0\`, \`s1\`, \`s2\` ... \`s63\`. Think of them as separate tracks on a mixing desk, each track with its own instruments and effects. A Stream is an instance of a [Stream class](/docs/classes#stream). 
+The [Stream class](/docs/classes#stream) represents separate musical layers, written as \`s0\`, \`s1\`, \`s2\` ... \`s63\`. Think of Streams as tracks on a mixing desk, each with its own instruments and effects.
 
-A Stream is just an object that returns a Pattern for each property. For example, \`s0.n\` returns a Pattern that can be used to set the note values for stream 0, \`s0.reverb\` returns a Pattern that can be used to set the reverb value for stream 0, and so on. Even, \`s0.banana\` will return a Pattern, though it's unlikely that you'll find a banana parameter in your synth engine.
+A Stream is just an object that returns a Pattern when you access a property. For example, \`s0.n\` returns a Pattern that can be used to set note values, whereas \`s0.reverb\` returns a Pattern that can be used to set the reverb, and so on. Any key will work: even \`s0.banana\` will return a Pattern, though it's unlikely that it will be useful.
 
-Properties can be set directly, as above, or using the \`.set()\` method, which accepts an object literal with key/value pairs. For example, these two examples are equivalent:
+You can set properties directly, as follows:
 \`\`\`js
 s0.inst.set('synth')
-s0.n.saw(2,0,32).add(48)
+s0.n.saw(2,48,64)
 s0.e.sometimes()
 \`\`\`
 
+Or you can set multiple properties at once using  the \`.set()\` method. The following example is equivalent to the previous one:
 \`\`\`js
-s0.set({inst: 'synth', n: saw(2,0,32).add(48), e: sometimes()})
+s0.set({inst: 'synth', n: saw(2,48,64).add(48), e: sometimes()})
 \`\`\`
 
-The properties you set depend on the parameters of the instruments and effect you are using. For example, if you are using the sampler, you'll want to set a sample bank and index: 
+Keys are mapped to the the instruments and effects in the synth engine. For example, if you are using the sampler, you'll want to set a sample bank and index: 
 \`\`\`js
-s0.set({inst: 'sampler', bank: 'bd808 sd808', e: sometimes().cache(16,4)})
+s0.set({inst: 'sampler', bank: 'bd808 sd808'})
 s0.i.random(1,15).step(1).cache(16,4)
+s0.e.sometimes().cache(16,4)
 \`\`\`
 
 You can find a full list of instruments and their parameters in the [Synths docs](/docs/synths), and a full list of effects and their parameters in the [Effects docs](/docs/fx).
@@ -25,47 +27,55 @@ You can find a full list of instruments and their parameters in the [Synths docs
 Here are some special stream properties.
 
 ## .e
-\`.e\` stands for event and is used to trigger the stream. If \`.e\` is set to 0 no event is triggered. If \`.e\` is greater than 0, an event is triggered. Consequently, there are many Pattern methods that simply return 1s and 0s.
+\`.e\` triggers events on the stream. If \`.e\` is greater than 0, an event is triggered. There are many Pattern methods that simply return 1s and 0s.
 
-Here are some different ways you could trigger a stream. Try replacing the final line of the previous example with one of the following:
+Here are some different ways you can trigger a stream. Try replacing the final line of the previous example with one of the following:
 \`\`\`js
-s0.e.every(4)
+s0.e.every(4).print('e')
 \`\`\`
 
 \`\`\`js
-s0.e.every(4).or(every(3))
+s0.e.every(4).or(every(3)).print('e')
 \`\`\`
 
 \`\`\`js
-s0.e.sine().step(1)
+s0.e.sine().step(1).print('e')
 \`\`\`
 
 \`\`\`js
-s0.e.set('1?0*16')
+s0.e.set('1?0*16').print('e')
 \`\`\`
 
 \`\`\`js
-s0.e.set('3:8*2')
-\`\`\`
-
-\`\`\`js
-s0.set({e: '1?0*16'})
+s0.e.set('3:8*2').print('e')
 \`\`\`
 
 ## .m
-\`.e\` stands for mutation and is used to modulate parameters whilst a synth is playing. We'll cover this in depth in a later chapter. It works the same way as \`.e\`, so you can use the same methods to set it. For example, \`s0.m.every(4)\` will modulate the stream every 4 beats.
+\`.m\` *mutates* parameters whilst a synth is playing. We'll cover this in depth in a [later chapter](/learn/mutations). It works in the same way as \`.e\`. The following example mutates the stream every 4 beats.
+\`\`\`js
+s0.m.every(4).print('m')
+\`\`\`
 
 ## .cut
-Cut is a special parameter that allows you to cut short active events in any stream. It accepts an integer or array of integers which reference the index of a stream. For example, \`s0.set({cut:0})\` will cut any active events in stream 0 short. \`s0.set({cut:[1,2]})\` will cut any active events in streams 1 and 2 short. By default, cut fades out the event over 5ms. You can set the fade time using the \`cutr\` parameter. For example, \`s0.set({cut:0,cutr:500})\` will cut any active events in stream 0 short over 500ms. 
+Cut is a special parameter that stops notes in the same or other streams when an event is triggered. You can pass one or more numbers referencing the streams to cut. For example, \`s0.set({cut:0})\` will cut notes in stream 0 short. Try commenting out the \`cut\` line in the following example:
+\`\`\`js
+s0.set({inst:'synth', n:'Ddor%16..*16'})
+s0.cut.set(0)
+s0.e.sometimes()
+\`\`\`
 
 ## .solo and .mute
-Solo and mute are used to solo or mute a stream. For example, \`s0.solo.set(1)\` will solo stream 0. \`s0.mute.set(1)\` will mute stream 0.
+Used to solo or mute a stream.
+\`\`\`js
+s0.solo.set(1) // solo stream 0
+s1.mute.set(1) // mute stream 1
+\`\`\`
 
 ## .level
-Level is a special parameter that allows you to control the level of the track used by the Stream. It accepts values between 0 and 1. For example, \`s0.set({level:0.5})\` will set the level of stream 0 to 0.5. Think of it as the final fader on a mixing desk channel strip.
+Set the level of the stream after all effects and filters. Think of it as the final fader on a mixing desk channel strip. Expects a value between 0 and 1.
 
 ## .out
-Streams are always stereo. By default, all streams are routed to the first two channels of your output device. You can route streams using the \`out\` parameter. For example, \`s0.set({out: 2})\` will route stream 0 to channels 2 and 3. Here's a shorthand way of spreading your streams across the outputs of your audio interface:
+By default, all streams are routed to the first two channels of your output device. You can route streams using the \`out\` parameter. For example, \`s0.set({out: 2})\` will route stream 0 to channels 2 and 3. Streams are always stereo. Here's a shorthand way of spreading your streams across the outputs of your audio interface:
 \`\`\`js
 streams.slice(0,8).map((stream,i) => stream.ps({out: i*2}))
 \`\`\`
