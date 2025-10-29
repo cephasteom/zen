af65524e02166c42f5f6a96e315fca994dd11f59
Peter Thomas <petethomasmusique@gmail.com>
2025-10-29 11:43:48 +0000

    main: add in correct adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@ptkdev/sveltekit-electron-adapter"; // for when you want to build an electron app
-// import adapter from '@sveltejs/adapter-auto';
+// import adapter from "@ptkdev/sveltekit-electron-adapter"; // for when you want to build an electron app
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { readFileSync } from 'fs';
